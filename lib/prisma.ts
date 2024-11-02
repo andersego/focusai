60596df6f0acd0a9e81809471073e0cca56628b7
Ander <ander@Administrators-MacBook-Pro.local>
2024-11-02 17:53:27 +0100

    Fix prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,39 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-interface QueryEvent {
-  query: string;
-  params: string;
-  duration: number;
-}
-
-const prisma = global.prisma || new PrismaClient({
-  log: [
-    {
-      emit: 'event',
-      level: 'query',
-    },
-    {
-      emit: 'stdout',
-      level: 'error',
-    },
-    {
-      emit: 'stdout',
-      level: 'info',
-    },
-    {
-      emit: 'stdout',
-      level: 'warn',
-    },
-  ],
-})
-
-// Debug logs
-prisma.$on('query', (e: QueryEvent) => {
-  console.log('Query: ' + e.query)
-  console.log('Params: ' + e.params)
-  console.log('Duration: ' + e.duration + 'ms')
-})
+const prisma = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma
